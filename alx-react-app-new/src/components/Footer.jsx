26ae7175c2a6919f2d046ad55933cc1d54408af7
oluwapelumi igbinlade <igbinladepelumi@gmail.com>
2025-07-14 08:47:54 +0100

    new react app

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#b744d5",
+        color: "white",
+        padding: "10px",
+        textAlign: "center",
+      }}
+    >
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        backgroundColor: "#ffa822",
+        padding: "30px",
+        textAlign: "center",
+        borderRadius: "20px",
+      }}
+    >
+      <p style={{ fontSize: "16px", color: "#333" }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+export default MainContent;
